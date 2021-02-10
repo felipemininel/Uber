9ab39c9098371f85ffabb4e7600a731f2f9912f5
Felipe <felipe351@gmail.com>
2021-02-10 18:34:12 -0300

    Initial Commit

---
+++ Uber/StatusCorrida.swift
@@ -0,0 +1,10 @@
+//
+//  StatusCorrida.swift
+//  Uber
+//
+//  Created by Felipe Mininel on 21/09/20.
+//
+
+enum StatusCorrida: String {
+    case EmRequisicao, PegarPassageiro, IniciarViagem, EmViagem, ViagemFinalizada
+}
